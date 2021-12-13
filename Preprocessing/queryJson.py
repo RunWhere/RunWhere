0dfa68e2e093e4408450fc0f49cf60fe186c08fb
Yong Liang <yongliangang@gmail.com>
2021-12-13 17:39:50 +0800

    Team meetup programming (Edge Generation)
    Co-authored-by: Jon <joncwr99@users.noreply.github.com>
    Co-authored-by: jeromewjj <jeromewjj@users.noreply.github.com>
    Co-authored-by: Hester James Douglas <phantl@users.noreply.github.com>

--- Preprocessing/queryJson.py
+++ Preprocessing/queryJson.py
@@ -1,6 +1,8 @@
 import json
 from math import asin, cos, radians, sin, sqrt
 
+fileName = ['edgeZoneA', 'edgeZoneB', 'edgeZoneC']
+    
 def distance(lat1, lon1, lat2, lon2):
      
     # The math module contains a function named
@@ -22,37 +24,79 @@ def distance(lat1, lon1, lat2, lon2):
       
     # calculate the result
     return(c * r)
-    
-with open("Data/verticles_edges.json") as data:
-    loaded = json.load(data)
-
-def testZone(zone):
-    for i in loaded[zone]:
-        print(i['name'])
-        print(i['latitude'])
-        print(i['longitude'])
-
 
 # Step 1 -> Form complete graph for each zone
 # store all of the complete graph in an edgelist
 # edgeListZoneA = [
 #     ["ZoneA","Faculty of Engineering","Temasek Lab",dist],
 # ]
-# Step 2
-# We select edges. + (BFS)
 
-# step 3 (Run Floyd Warshall)
-## distance space o(V^2) * 3
-## path array space o(v^2) * 3
+'''
+def completeGraphEdgeGen(zoneX, jsonData):
+    zoneX <- jsonData['zoneX']
+    completeEdgeZoneX = [][]
+    ## Filter duplicated edges
+    for i in range(0,len(zoneX)): # all the vertices
+        for k in range(i + 1, len(zoneX)):
+            tempDist <- distance(zoneX[i].lat, zoneX[i].lon, zoneX[k].lat, zoneX[k].lon)
+            completeEdgeZoneX.append([zoneX, zoneX[i].name, zoneX[k].name, tempDist])
 
-row = [
-    [["Faculty of Engineering",0],["Temasek Lab", 0.18]], 
-    [["Faculty of Engineering",0.18],["Temasek Lab", 0]]
-]
+    return completeEdgeZoneX
+'''
 
-with open("Data/test.json", 'w') as outfile:
-    print(json.dump(row, outfile))
+def completeGraphEdgeGen(zone, jsonData):
+    zoneX = jsonData[str(zone)]
+    completeEdgeZoneX = []
+
+    # all the vertices
+    for i in range(0,len(zoneX)):
+
+        ## Filter duplicated edges
+        for k in range(i + 1, len(zoneX)):
+
+            tempDist = distance(zoneX[i]['latitude'], zoneX[i]['longitude'], zoneX[k]['latitude'], zoneX[k]['longitude'])
+            completeEdgeZoneX.append([zone, zoneX[i]['name'], zoneX[k]['name'], tempDist])
+
+    return completeEdgeZoneX
+
+def createFile(zoneX, edgeZoneX):
+    with open("Data/{}.json".format(zoneX), 'w') as outfile:
+        json.dump(edgeZoneX, outfile, indent=4)
 
 
-# testZone('ZoneC')
-# print(distance(loaded['ZoneA'][0]['latitude'], loaded['ZoneA'][0]['longitude'], loaded['ZoneA'][1]['latitude'], loaded['ZoneA'][1]['longitude']))
+def exportEdge():
+
+    with open("Data/verticles_edges.json") as data:
+        loaded = json.load(data)
+        
+    edgeZoneA = completeGraphEdgeGen('ZoneA',loaded)
+    edgeZoneB = completeGraphEdgeGen('ZoneB',loaded)
+    edgeZoneC = completeGraphEdgeGen('ZoneC',loaded)
+    
+    edgeData = [edgeZoneA, edgeZoneB, edgeZoneC]
+
+    for i in range(len(fileName)):
+        createFile(fileName[i], edgeData[i])
+    
+
+
+def testEdgeExistence():
+    
+    # Running the test to see if there is edges being produced
+    for i in range(len(fileName)):
+        with open("Data/{}.json".format(fileName[i])) as testfile:
+            loaded = json.load(testfile)
+
+        for i in loaded:
+            print(i)
+
+def preprocessing():
+    exportEdge()
+    testEdgeExistence()
+
+
+preprocessing()
+
+#  (Greedy BFS) for 3 options (2.4km, 5km and 10km)
+# # distance space o(V^2) * 3
+# # path array space o(v^2) * 3
